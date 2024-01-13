6b89e5747205a5bb20dac2c7369440df8b3260c3
tugalsan karabacak <tugalsan@users.noreply.github.com>
2024-01-14 01:12:28 +0300

    Update TS_SURLHandler02ForFileDownload.java

--- src/main/java/com/tugalsan/api/servlet/url/server/handler/TS_SURLHandler02ForFileDownload.java
+++ src/main/java/com/tugalsan/api/servlet/url/server/handler/TS_SURLHandler02ForFileDownload.java
@@ -1,5 +1,6 @@
 package com.tugalsan.api.servlet.url.server.handler;
 
+import com.tugalsan.api.log.server.TS_Log;
 import com.tugalsan.api.unsafe.client.TGS_UnSafe;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -7,7 +8,8 @@ import javax.servlet.http.HttpServletResponse;
 
 public class TS_SURLHandler02ForFileDownload extends TS_SURLHandler02ForAbstract {
 
-//    final private static TS_Log d = TS_Log.of(TS_SURLHandler02ForFileDownload.class);
+    final private static TS_Log d = TS_Log.of(TS_SURLHandler02ForFileDownload.class);
+
     private TS_SURLHandler02ForFileDownload(HttpServlet hs, HttpServletRequest rq, HttpServletResponse rs, boolean noCache) {
         super(hs, rq, rs, noCache);
     }
