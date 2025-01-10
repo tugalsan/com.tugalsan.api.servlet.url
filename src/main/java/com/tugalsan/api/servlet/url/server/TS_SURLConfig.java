b37feb283002619b78e0b76103055ea49cd9cafa
tugalsan karabacak <tugalsan@users.noreply.github.com>
2025-01-10 06:33:50 +0300

    Update TS_SURLConfig.java

--- src/main/java/com/tugalsan/api/servlet/url/server/TS_SURLConfig.java
+++ src/main/java/com/tugalsan/api/servlet/url/server/TS_SURLConfig.java
@@ -5,6 +5,7 @@ import com.tugalsan.api.file.server.TS_DirectoryUtils;
 import com.tugalsan.api.file.server.TS_FileUtils;
 import com.tugalsan.api.file.txt.server.TS_FileTxtUtils;
 import com.tugalsan.api.log.server.TS_Log;
+import com.tugalsan.api.union.client.TGS_UnionExcuse;
 import com.tugalsan.api.unsafe.client.TGS_UnSafe;
 import java.io.Serializable;
 import java.nio.file.Path;
@@ -12,10 +13,10 @@ import java.util.Properties;
 
 public class TS_SURLConfig implements Serializable {
 
-    final private static TS_Log d = TS_Log.of(TS_SURLConfig.class);
+    final private static TS_Log d = TS_Log.of(true, TS_SURLConfig.class);
     final private static boolean DEFAULT_ENABLE_TIMEOUT = true;
 
-    private TS_SURLConfig(){//DTO
+    private TS_SURLConfig() {//DTO
     }
 
     private TS_SURLConfig(boolean enableTimeout) {
@@ -67,16 +68,20 @@ public class TS_SURLConfig implements Serializable {
         enableTimeout = (Boolean) prop.getOrDefault("enableTimeout", enableTimeout);
     }
 
-    public static TS_SURLConfig of(Path dir, String appName) {
+    public static TGS_UnionExcuse<TS_SURLConfig> of(Path dir, String appName) {
         TS_DirectoryUtils.assureExists(dir);
         var filePath = dir.resolve(TS_SURLConfig.class.getSimpleName() + "." + appName + ".json");
         d.cr("of", filePath);
 
         if (!TS_FileUtils.isExistFile(filePath)) {
+            d.ci("of", "file-not-exists");
             TS_DirectoryUtils.createDirectoriesIfNotExists(filePath.getParent());
             var tmp = TS_SURLConfig.of();
             var jsonString = TS_FileJsonUtils.toJSON(tmp, true);
             TS_FileJsonUtils.toFile(jsonString, filePath, false, true);
+            d.ci("of", "file-not-exists", "file created");
+        } else {
+            d.ci("of", "file-exists");
         }
 
         var jsonString = TGS_UnSafe.call(() -> TS_FileTxtUtils.toString(filePath), e -> {
@@ -87,8 +92,10 @@ public class TS_SURLConfig implements Serializable {
             TS_FileTxtUtils.toFile(jsonString0, filePath, false);
             return jsonString0;
         });
-        d.ci("of", jsonString);
+        d.ci("of", "jsonString", jsonString);
 
-        return TS_FileJsonUtils.toObject(jsonString, TS_SURLConfig.class);
+        var obj = TS_FileJsonUtils.toObject(jsonString, TS_SURLConfig.class);
+        d.ci("of", "obj constructoed", obj);
+        return obj;
     }
 }
